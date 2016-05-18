07897989aba2c95fb27c0f2017a961584e8b5e4e
Francisco López Pérez <fcolope92@gmail.com>
2016-05-19 00:46:26 +0200

    Finish RecyclerView, Drawer, NavBar

--- app/src/main/java/com/example/android/materialdesign/MainActivity.java
+++ app/src/main/java/com/example/android/materialdesign/MainActivity.java
@@ -1,24 +1,25 @@
 package com.example.android.materialdesign;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.CardView;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -28,12 +29,15 @@ public class MainActivity extends AppCompatActivity {
     private ActionBar actionBar;
     private RecyclerView recyclerView;
     private LinearLayoutManager llm;
-    private GridLayoutManager glm;
+    private boolean changeView;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        changeView = false;
 
         /************** ACTION BAR **************/
         toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -45,10 +49,12 @@ public class MainActivity extends AppCompatActivity {
 
         drawerLayout = (DrawerLayout) findViewById(R.id.navigation_drawer_layout);
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
         if (navigationView != null) {
             setupNavigationDrawerContent(navigationView);
         }
+
+        insertUserIcon();
 
         setupNavigationDrawerContent(navigationView);
 
@@ -56,34 +62,44 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = (RecyclerView)findViewById(R.id.rv);
         recyclerView.setHasFixedSize(true);
 
-        //glm = new GridLayoutManager(this, 2);
-
         llm = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(llm);
 
         AdapterCategories adapter = new AdapterCategories(initializeCategories());
         recyclerView.setAdapter(adapter);
+
+
+
     }
+
+
+    private void insertUserIcon() {
+        View header = LayoutInflater.from(this).inflate(R.layout.navigation_drawer_header, null);
+        navigationView.addHeaderView(header);
+        ImageView ivUserPhoto = (ImageView) header.findViewById(R.id.ivUserPhoto);
+
+        Bitmap icon = BitmapFactory.decodeResource(this.getResources(),
+                R.drawable.perfil);
+        icon = Bitmap.createScaledBitmap(icon, 128, 128, true);
+        Bitmap bm = ImageConverter.getRoundedCornerBitmap(icon, 1000);
+        ivUserPhoto.setImageBitmap(bm);
+    }
+
+
 
     private ArrayList<Category> initializeCategories() {
         ArrayList<Category> categories = new ArrayList<>();
         categories.add(new Category("Floating Action Button", "Floating Action Button Floating Action Button Floating Action Button Floating Action Button"));
+        categories.add(new Category("Floating Action Button", "Floating Action Button"));
         categories.add(new Category("Floating Action Button", "Floating Action Button Floating Action Button Floating Action Button Floating Action Button"));
         categories.add(new Category("Floating Action Button", "Floating Action Button Floating Action Button Floating Action Button Floating Action Button"));
+        categories.add(new Category("Floating Action Button", "Floating Action Button"));
         categories.add(new Category("Floating Action Button", "Floating Action Button Floating Action Button Floating Action Button Floating Action Button"));
         categories.add(new Category("Floating Action Button", "Floating Action Button Floating Action Button Floating Action Button Floating Action Button"));
-        categories.add(new Category("Floating Action Button", "Floating Action Button Floating Action Button Floating Action Button Floating Action Button"));
-        categories.add(new Category("Floating Action Button", "Floating Action Button Floating Action Button Floating Action Button Floating Action Button"));
-        categories.add(new Category("Floating Action Button", "Floating Action Button Floating Action Button Floating Action Button Floating Action Button"));
-        categories.add(new Category("Floating Action Button", "Floating Action Button Floating Action Button Floating Action Button Floating Action Button"));
-
+        categories.add(new Category("Floating Action Button", "Floating Action Button"));
+        categories.add(new Category("Floating Action Button", "Floating Action Button"));
 
         return categories;
-    }
-
-    public void tostada() {
-
-        Toast.makeText(this, "¯\\_(ツ)_/¯", Toast.LENGTH_LONG).show();
     }
 
     @Override
@@ -97,6 +113,20 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case android.R.id.home:
                 drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            case R.id.changeView:
+                if (!changeView) {
+                    item.setIcon(R.drawable.ic_view_agenda_white_24dp);
+                    StaggeredGridLayoutManager gridLayoutManager =
+                            new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+                    recyclerView.setLayoutManager(gridLayoutManager);
+                    changeView = true;
+                } else {
+                    item.setIcon(R.drawable.ic_dashboard_white_24dp);
+                    llm = new LinearLayoutManager(this);
+                    recyclerView.setLayoutManager(llm);
+                    changeView = false;
+                }
                 return true;
         }
         return super.onOptionsItemSelected(item);
@@ -143,4 +173,5 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
     }
+
 }
